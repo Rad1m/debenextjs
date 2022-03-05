39fc8ea394cbfe918c7527173436247116a710e4
Rad1m <radim.simanek@protonmail.ch>
2022-03-05 19:18:49 +0000

    simple wallet button

--- pages/helpers/utilities.js
+++ pages/helpers/utilities.js
@@ -1,3 +1,5 @@
+// helper utilities
+
 export function ellipseAddress(address, width) {
   const concatAddress = address.slice(0, width) + "..." + address.slice(-width);
   console.log("Loggin address %s", concatAddress);
